78c868de9c5a439d9dc80f4d1978b08b671452f0
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-22 13:44:05 -0600

    Finished exercise.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const $ = require('jquery');
 
-let sayHello = () => {
-    console.log('Hello');
-};
+// let sayHello = () => {
+//     console.log('Hello');
+// };
+//
+// sayHello();
+//
+// $(document).ready(function(){
+//     console.log("Ready!");
+// });
 
+import {sayHello} from "./say-hello";
 sayHello();
-
-$(document).ready(function(){
-    console.log("Ready!");
-});
---
+++ src/say-hello.js
@@ -0,0 +1,4 @@
+export let sayHello = () => {
+    console.log('Hello there!');
+};
+
